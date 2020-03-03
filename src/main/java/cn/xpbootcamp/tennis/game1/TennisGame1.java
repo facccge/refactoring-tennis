057726c7f2d6dad9d0423d19ffe966710a4382cc
facccge <fcfc5702@qq.com>
2020-03-03 23:30:02 +0800

    TennisGame1: Extract methods to solve long method.

--- src/main/java/cn/xpbootcamp/tennis/game1/TennisGame1.java
+++ src/main/java/cn/xpbootcamp/tennis/game1/TennisGame1.java
@@ -23,52 +23,70 @@ public class TennisGame1 implements TennisGame {
 
     public String getScore() {
         StringBuilder score = new StringBuilder();
-        int tempScore;
         if (m_score1 == m_score2) {
-            switch (m_score1) {
+            score.append(buildScoreWhenEqual());
+        } else if (m_score1 >= 4 || m_score2 >= 4) {
+            score.append(buildScoreWhenBothGreaterThan4());
+        } else {
+            score.append(buildScoreForNormalCase());
+        }
+        return score.toString();
+    }
+
+    private String buildScoreForNormalCase() {
+        StringBuilder score = new StringBuilder();
+        int tempScore;
+        for (int i = 1; i < 3; i++) {
+            if (i == 1) tempScore = m_score1;
+            else {
+                score.append("-");
+                tempScore = m_score2;
+            }
+            switch (tempScore) {
                 case 0:
-                    score = new StringBuilder("Love-All");
+                    score.append("Love");
                     break;
                 case 1:
-                    score = new StringBuilder("Fifteen-All");
+                    score.append("Fifteen");
                     break;
                 case 2:
-                    score = new StringBuilder("Thirty-All");
+                    score.append("Thirty");
                     break;
-                default:
-                    score = new StringBuilder("Deuce");
+                case 3:
+                    score.append("Forty");
                     break;
-
-            }
-        } else if (m_score1 >= 4 || m_score2 >= 4) {
-            int minusResult = m_score1 - m_score2;
-            if (minusResult == 1) score = new StringBuilder("Advantage " + player1Name);
-            else if (minusResult == -1) score = new StringBuilder("Advantage " + player2Name);
-            else if (minusResult >= 2) score = new StringBuilder("Win for " + player1Name);
-            else score = new StringBuilder("Win for " + player2Name);
-        } else {
-            for (int i = 1; i < 3; i++) {
-                if (i == 1) tempScore = m_score1;
-                else {
-                    score.append("-");
-                    tempScore = m_score2;
-                }
-                switch (tempScore) {
-                    case 0:
-                        score.append("Love");
-                        break;
-                    case 1:
-                        score.append("Fifteen");
-                        break;
-                    case 2:
-                        score.append("Thirty");
-                        break;
-                    case 3:
-                        score.append("Forty");
-                        break;
-                }
             }
         }
         return score.toString();
+    }
+
+    private StringBuilder buildScoreWhenBothGreaterThan4() {
+        StringBuilder score;
+        int minusResult = m_score1 - m_score2;
+        if (minusResult == 1) score = new StringBuilder("Advantage " + player1Name);
+        else if (minusResult == -1) score = new StringBuilder("Advantage " + player2Name);
+        else if (minusResult >= 2) score = new StringBuilder("Win for " + player1Name);
+        else score = new StringBuilder("Win for " + player2Name);
+        return score;
+    }
+
+    private StringBuilder buildScoreWhenEqual() {
+        StringBuilder score;
+        switch (m_score1) {
+            case 0:
+                score = new StringBuilder("Love-All");
+                break;
+            case 1:
+                score = new StringBuilder("Fifteen-All");
+                break;
+            case 2:
+                score = new StringBuilder("Thirty-All");
+                break;
+            default:
+                score = new StringBuilder("Deuce");
+                break;
+
+        }
+        return score;
     }
 }
